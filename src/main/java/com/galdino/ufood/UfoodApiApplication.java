9c7d6b9a6a4b14a5eabe91eb40d4ee788c77b3fc
Fernando Galdino <fernando.galdino@meta.com.br>
2023-08-18 15:50:02 -0300

    add Spring Data JPA and database connector

---
+++ src/main/java/com/galdino/ufood/UfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.galdino.ufood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfoodApiApplication.class, args);
+	}
+
+}
