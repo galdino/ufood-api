fe431520906710e5d1bf99dec67715c4710e7dca
Fernando Galdino <fgaldinoo@gmail.com>
2023-08-31 17:23:10 -0300

    use lombok

--- src/main/java/com/galdino/ufood/domain/model/Kitchen.java
+++ src/main/java/com/galdino/ufood/domain/model/Kitchen.java
@@ -1,32 +1,22 @@
 package com.galdino.ufood.domain.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 import java.util.Objects;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Kitchen {
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     @Override
     public boolean equals(Object o) {
