d48b10bc658339fb3ca38a942fb1058e618a34c9
Fernando Galdino <fgaldinoo@gmail.com>
2023-09-11 18:36:52 -0300

    content negotiation for JSON or XML

--- src/main/java/com/galdino/ufood/api/controller/KitchenController.java
+++ src/main/java/com/galdino/ufood/api/controller/KitchenController.java
@@ -2,6 +2,7 @@ package com.galdino.ufood.api.controller;
 
 import com.galdino.ufood.domain.model.Kitchen;
 import com.galdino.ufood.domain.repository.KitchenRepository;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/kitchens")
+@RequestMapping(value = "/kitchens", produces = MediaType.APPLICATION_JSON_VALUE)
 public class KitchenController {
 
     private KitchenRepository kitchenRepository;
