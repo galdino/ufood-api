1f3841735408b07f274d2c37c2027d2ca3245da3
Fernando Galdino <fgaldinoo@gmail.com>
2023-09-11 18:54:43 -0300

    singleton resource with GET

--- src/main/java/com/galdino/ufood/api/controller/KitchenController.java
+++ src/main/java/com/galdino/ufood/api/controller/KitchenController.java
@@ -4,6 +4,7 @@ import com.galdino.ufood.domain.model.Kitchen;
 import com.galdino.ufood.domain.repository.KitchenRepository;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,10 @@ public class KitchenController {
     @GetMapping
     public List<Kitchen> list() {
         return kitchenRepository.list();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Kitchen findById(@PathVariable Long id) {
+        return kitchenRepository.findById(id);
     }
 }
