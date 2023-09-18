5d240934fb384150f10cae8d90ab899c6b69164e
Fernando Galdino <fgaldinoo@gmail.com>
2023-09-18 17:50:53 -0300

    add resources with POST method

--- src/main/java/com/galdino/ufood/api/controller/KitchenController.java
+++ src/main/java/com/galdino/ufood/api/controller/KitchenController.java
@@ -3,12 +3,10 @@ package com.galdino.ufood.api.controller;
 import com.galdino.ufood.api.model.KitchensXmlWrapper;
 import com.galdino.ufood.domain.model.Kitchen;
 import com.galdino.ufood.domain.repository.KitchenRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -52,5 +50,12 @@ public class KitchenController {
         }
 
         return ResponseEntity.ok(kitchen);
+    }
+
+    @PostMapping
+    public ResponseEntity<Kitchen> add(@RequestBody Kitchen kitchen) {
+        Kitchen kitchenAux = kitchenRepository.add(kitchen);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(kitchenAux);
     }
 }
