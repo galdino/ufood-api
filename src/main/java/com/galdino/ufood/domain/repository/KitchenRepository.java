2886cfd1a4642490cc95021a138aaf1547ca58c7
Fernando Galdino <fgaldinoo@gmail.com>
2023-10-05 17:10:22 -0300

    use query methods

--- src/main/java/com/galdino/ufood/domain/repository/KitchenRepository.java
+++ src/main/java/com/galdino/ufood/domain/repository/KitchenRepository.java
@@ -4,7 +4,9 @@ import com.galdino.ufood.domain.model.Kitchen;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface KitchenRepository extends JpaRepository<Kitchen, Long> {
-//    List<Kitchen> findByName(String name);
+    List<Kitchen> findByName(String name);
 }
