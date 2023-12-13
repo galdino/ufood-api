447bbbce74a0a80acbf03b5f41596489514114f9
Fernando Galdino <fgaldinoo@gmail.com>
2023-12-13 11:13:02 -0300

    use @NotBlank and @PositiveOrZero annotations

--- src/main/java/com/galdino/ufood/domain/model/Restaurant.java
+++ src/main/java/com/galdino/ufood/domain/model/Restaurant.java
@@ -7,7 +7,8 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -23,10 +24,14 @@ public class Restaurant {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
+//    @NotNull
+//    @NotEmpty
+    @NotBlank
     @Column(nullable = false)
     private String name;
 
+//    @DecimalMin("0")
+    @PositiveOrZero
     @Column(name = "delivery_fee", nullable = false)
     private BigDecimal deliveryFee;
 
