fb9e12bed8b0fe128253676af8b150826cabb689
Fernando Galdino <fgaldinoo@gmail.com>
2024-01-03 18:17:21 -0300

    add api tests with RestAssured

---
+++ src/test/java/com/galdino/ufood/KitchenControllerIT.java
@@ -0,0 +1,35 @@
+package com.galdino.ufood;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.given;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class KitchenControllerIT {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void shouldReturnStatus200_WhenGetKitchens() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+            given()
+                .basePath("/kitchens")
+                .port(port)
+                .accept(ContentType.JSON)
+            .when()
+                .get()
+            .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+}
