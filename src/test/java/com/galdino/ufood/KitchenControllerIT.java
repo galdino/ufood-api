ac9ba98a1b5d0d3d6b37b8ca536534afdcc61bae
Fernando Galdino <fgaldinoo@gmail.com>
2024-01-05 17:46:35 -0300

    use migrate to reset db before each test

--- src/test/java/com/galdino/ufood/KitchenControllerIT.java
+++ src/test/java/com/galdino/ufood/KitchenControllerIT.java
@@ -2,10 +2,12 @@ package com.galdino.ufood;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -20,11 +22,16 @@ public class KitchenControllerIT {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private Flyway flyway;
+
     @Before
     public void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/kitchens";
+
+        flyway.migrate();
     }
 
     @Test
