ab95eb266e087dca54ec5268298f2daa1fdfc7c3
Fernando Galdino <fgaldinoo@gmail.com>
2024-01-05 17:23:05 -0300

    use @Before annotation for testing

--- src/test/java/com/galdino/ufood/KitchenControllerIT.java
+++ src/test/java/com/galdino/ufood/KitchenControllerIT.java
@@ -3,6 +3,7 @@ package com.galdino.ufood;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,13 +20,16 @@ public class KitchenControllerIT {
     @LocalServerPort
     private int port;
 
+    @Before
+    public void setUp() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.port = port;
+        RestAssured.basePath = "/kitchens";
+    }
+
     @Test
     public void shouldReturnStatus200_WhenGetKitchens() {
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-
             given()
-                .basePath("/kitchens")
-                .port(port)
                 .accept(ContentType.JSON)
             .when()
                 .get()
@@ -35,18 +39,13 @@ public class KitchenControllerIT {
 
     @Test
     public void shouldHave2Kitchens_WhenGetKitchens() {
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-
             given()
-                .basePath("/kitchens")
-                .port(port)
                 .accept(ContentType.JSON)
             .when()
                 .get()
             .then()
                 .body("", Matchers.hasSize(2))
                 .body("name", Matchers.hasItems("Thai", "Indian"));
-
     }
 
 }
