51740e9cd370b3894edb2dfcfe12ff183c1e0b83
Fernando Galdino <fgaldinoo@gmail.com>
2024-02-23 17:46:12 -0300

    use ModelMapper for assembler and disassembler

--- src/main/java/com/galdino/ufood/api/assembler/RestaurantInputDiassembler.java
+++ src/main/java/com/galdino/ufood/api/assembler/RestaurantInputDiassembler.java
@@ -1,24 +1,23 @@
 package com.galdino.ufood.api.assembler;
 
 import com.galdino.ufood.api.model.RestaurantInput;
-import com.galdino.ufood.domain.model.Kitchen;
 import com.galdino.ufood.domain.model.Restaurant;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestaurantInputDiassembler {
 
+    private ModelMapper modelMapper;
+
+    public RestaurantInputDiassembler(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
     public Restaurant toDomainObject(RestaurantInput restaurantInput) {
-        Restaurant restaurant = new Restaurant();
-        restaurant.setName(restaurantInput.getName());
-        restaurant.setDeliveryFee(restaurantInput.getDeliveryFee());
 
-        Kitchen kitchen = new Kitchen();
-        kitchen.setId(restaurantInput.getKitchen().getId());
+        return modelMapper.map(restaurantInput, Restaurant.class);
 
-        restaurant.setKitchen(kitchen);
-
-        return restaurant;
     }
 
 }
--- src/main/java/com/galdino/ufood/api/assembler/RestaurantModelAssembler.java
+++ src/main/java/com/galdino/ufood/api/assembler/RestaurantModelAssembler.java
@@ -1,8 +1,8 @@
 package com.galdino.ufood.api.assembler;
 
-import com.galdino.ufood.api.model.KitchenModel;
 import com.galdino.ufood.api.model.RestaurantModel;
 import com.galdino.ufood.domain.model.Restaurant;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,18 +11,14 @@ import java.util.stream.Collectors;
 @Component
 public class RestaurantModelAssembler {
 
+    private ModelMapper modelMapper;
+
+    public RestaurantModelAssembler(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
     public RestaurantModel toModel(Restaurant restaurant) {
-        KitchenModel kitchenModel = new KitchenModel();
-        kitchenModel.setId(restaurant.getKitchen().getId());
-        kitchenModel.setName(restaurant.getKitchen().getName());
-
-        RestaurantModel restaurantModel = new RestaurantModel();
-        restaurantModel.setId(restaurant.getId());
-        restaurantModel.setName(restaurant.getName());
-        restaurantModel.setDeliveryFee(restaurant.getDeliveryFee());
-        restaurantModel.setKitchen(kitchenModel);
-
-        return restaurantModel;
+        return modelMapper.map(restaurant, RestaurantModel.class);
     }
 
     public List<RestaurantModel> toCollectionModel(List<Restaurant> restaurants) {
