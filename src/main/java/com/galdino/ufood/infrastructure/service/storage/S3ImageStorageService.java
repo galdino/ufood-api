ed0da67943ef2f4933fdfec0a8ec0f5845994da1
Fernando Galdino <fgaldinoo@gmail.com>
2024-10-01 17:54:47 -0300

    add Amazon S3 remove for image

--- src/main/java/com/galdino/ufood/infrastructure/service/storage/S3ImageStorageService.java
+++ src/main/java/com/galdino/ufood/infrastructure/service/storage/S3ImageStorageService.java
@@ -2,6 +2,7 @@ package com.galdino.ufood.infrastructure.service.storage;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.galdino.ufood.core.validation.storage.StorageProperties;
@@ -43,7 +44,15 @@ public class S3ImageStorageService implements ImageStorageService {
 
     @Override
     public void remove(String fileName) {
+        try {
+            String filePath = getFilePath(fileName);
 
+            DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(storageProperties.getS3().getBucket(), filePath);
+
+            amazonS3.deleteObject(deleteObjectRequest);
+        } catch (Exception e) {
+            throw new StorageException("Image Amazon S3 remove error.", e);
+        }
     }
 
     @Override
