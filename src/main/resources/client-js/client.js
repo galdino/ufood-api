e05ae4a42835c0258c0dd942d9f01839c0dc89af
Fernando Galdino <fgaldinoo@gmail.com>
2024-11-21 18:11:31 -0300

    change JS client

--- src/main/resources/client-js/client.js
+++ src/main/resources/client-js/client.js
@@ -1,12 +1,28 @@
-function getAllRestaurants() {
+function find() {
     $.ajax({
-        url: "http://localhost:8080/restaurants",
+        url: "http://localhost:8080/payment-methods",
         type: "get",
 
         success: function(response) {
-            $("#content").text(JSON.stringify(response));
+            writeTable(response);
         }
     });
 }
 
-$("#button").click(getAllRestaurants);
+function writeTable(paymentMethods) {
+    $("#pm-table tbody tr").remove();
+
+    $.each(paymentMethods, function(i, paymentMethod) {
+        var line = $("<tr>");
+
+        line.append(
+            $("<td>").text(paymentMethod.id),
+            $("<td>").text(paymentMethod.description)
+        );
+
+        line.appendTo("#pm-table");
+    });
+}
+
+
+$("#btn-find").click(find);
