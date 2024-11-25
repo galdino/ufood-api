2923a32c04bac5e144f7fc15302e5f61d913c802
Fernando Galdino <fgaldinoo@gmail.com>
2024-11-25 17:39:57 -0300

    create form for payment method

--- src/main/resources/client-js/client.js
+++ src/main/resources/client-js/client.js
@@ -9,6 +9,35 @@ function find() {
     });
 }
 
+function create() {
+    var paymentMethodJson = JSON.stringify({
+        "descricao": $("#desc-field").val()
+    });
+
+    console.log(paymentMethodJson);
+
+    $.ajax({
+        url: "http://localhost:8080/payment-methods",
+        type: "post",
+        data: paymentMethodJson,
+        contentType: "application/json",
+
+        success: function(response) {
+            alert("Payment Method created!");
+            find();
+        },
+
+        error: function(error) {
+            if (error.status == 400) {
+                var problem = JSON.parse(error.responseText);
+                alert(problem.userMessage);
+            } else {
+                alert("Payment Method creation error!");
+            }
+        }
+    });
+}
+
 function writeTable(paymentMethods) {
     $("#pm-table tbody tr").remove();
 
@@ -26,3 +55,4 @@ function writeTable(paymentMethods) {
 
 
 $("#btn-find").click(find);
+$("#btn-create").click(create);
