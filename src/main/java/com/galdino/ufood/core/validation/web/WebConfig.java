be8a0c56ea211997cc064054ad23e690dd441b0c
Fernando Galdino <fgaldinoo@gmail.com>
2024-12-09 18:29:57 -0300

    enable ETags

--- src/main/java/com/galdino/ufood/core/validation/web/WebConfig.java
+++ src/main/java/com/galdino/ufood/core/validation/web/WebConfig.java
@@ -1,8 +1,12 @@
 package com.galdino.ufood.core.validation.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
 
 @Component
 public class WebConfig implements WebMvcConfigurer {
@@ -13,5 +17,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*");
 //                .allowedOrigins("*")
 //                .maxAge(30);
+    }
+
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
     }
 }
