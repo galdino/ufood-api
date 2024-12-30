d0caaea5f6d4d0c008f61d43688e838dac13877a
Fernando Galdino <fgaldinoo@gmail.com>
2024-12-30 18:34:40 -0300

    customize swagger-ui.html

--- src/main/java/com/galdino/ufood/core/validation/openapi/SpringFoxConfig.java
+++ src/main/java/com/galdino/ufood/core/validation/openapi/SpringFoxConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,8 +19,18 @@ public class SpringFoxConfig implements WebMvcConfigurer {
     @Bean
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                                                      .apis(RequestHandlerSelectors.any())
-                                                      .build();
+                                                        .apis(RequestHandlerSelectors.basePackage("com.galdino.ufood.api"))
+//                                                      .paths(PathSelectors.ant("/restaurants/*"))
+                                                        .build()
+                                                      .apiInfo(apiInfo());
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Ufood API")
+                                   .description("Public API for customers and restaurants")
+                                   .version("1.0")
+                                   .contact(new Contact("Ufood", "https://www.ufood.com", "info@ufood.com"))
+                                   .build();
     }
 
     @Override
