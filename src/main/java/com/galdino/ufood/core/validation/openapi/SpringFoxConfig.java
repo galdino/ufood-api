f39191023ef3c655d82ea025501295a9684bca54
Fernando Galdino <fgaldinoo@gmail.com>
2025-01-08 18:19:33 -0300

    customize swagger-ui response codes

--- src/main/java/com/galdino/ufood/core/validation/openapi/SpringFoxConfig.java
+++ src/main/java/com/galdino/ufood/core/validation/openapi/SpringFoxConfig.java
@@ -3,17 +3,23 @@ package com.galdino.ufood.core.validation.openapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -26,11 +32,25 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                                                         .apis(RequestHandlerSelectors.basePackage("com.galdino.ufood.api"))
 //                                                      .paths(PathSelectors.ant("/restaurants/*"))
                                                         .build()
+                                                      .useDefaultResponseMessages(false)
+                                                      .globalResponseMessage(RequestMethod.GET, globalGetResponseMessages())
                                                       .apiInfo(apiInfo())
                                                       .tags(new Tag("Cities", "Operations about cities"));
     }
 
-    public ApiInfo apiInfo() {
+    private List<ResponseMessage> globalGetResponseMessages() {
+        return List.of(new ResponseMessageBuilder()
+                           .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                           .message("Internal Server Error")
+                           .build(),
+                       new ResponseMessageBuilder()
+                           .code(HttpStatus.NOT_ACCEPTABLE.value())
+                           .message("Consumer representation not acceptable")
+                           .build()
+                      );
+    }
+
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder().title("Ufood API")
                                    .description("Public API for customers and restaurants")
                                    .version("1.0")
