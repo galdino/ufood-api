2d7d567da82f64e65293357a27dbf2e76862de37
Fernando Galdino <fgaldinoo@gmail.com>
2025-04-09 18:33:25 -0300

    set authentication with in-memory users

--- src/main/java/com/galdino/ufood/core/validation/security/WebSecurityConfig.java
+++ src/main/java/com/galdino/ufood/core/validation/security/WebSecurityConfig.java
@@ -1,14 +1,30 @@
 package com.galdino.ufood.core.validation.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("ufood")
+                .password(passwordEncoder().encode("123"))
+                .roles("admin")
+            .and()
+            .withUser("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("admin");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -22,5 +38,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
                 .csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
